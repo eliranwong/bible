1eb20d43108fa994ce3566f0a2bc0f265a672b30
Eliran Wong <hkwebtech@gmail.com>
2019-08-21 14:07:50 +0100

    Add files via upload

--- bin/main.dart
+++ bin/main.dart
@@ -1,13 +1,36 @@
+/*
+The command line version is taking shape ...
+
+At the moment , you may try commands like:
+
+To open John 3:16 in KJV bible:
+* dart bin/main.dart open KJV John 3:16
+
+To open multiple verses (e.g. John 3:16-18, Rom 5:8, 3:23, 2Ti 3:14-16, Ge 1:5, 8, 13) in KJV bible:
+* dart bin/main.dart open KJV John 3:16-18, Rom 5:8, 3:23, 2Ti 3:14-1, Ge 1:5, 8, 13
+
+To search for verses containing "Christ Jesus":
+* dart bin/main.dart search KJV Christ Jesus
+
+To search for verses containing "Christ", followed by "Jesus" anywhere in the same verse:
+* dart bin/main.dart search KJV Christ.*?Jesus
+
+To compare John 3:16 in all installed bibles:
+* dart bin/main.dart compare ALL John 3:16
+
+To compare John 3:16 only in CUV and KJV:
+* dart bin/main.dart compare CUV_KJV John 3:16
+
+<i><b>Remarks:</b></i>
+* Please use "," instead of ";" to separate verses in command lines.
+* Common abbreviations are supported.
+*/
+
+
 import 'package:cli/BibleParser.dart';
 import 'package:cli/Bibles.dart';
 
 var bible1, bible2;
-
-// try commands like:
-// "bin/main.dart open KJV John 3:16"
-// "bin/main.dart open KJV John 3:16-18; Rom 5:8, 3:23, 25"
-// "bin/main.dart search KJV Christ Jesus" to search for verses containing "Christ Jesus"
-// "bin/main.dart search KJV Christ.*?Jesus" to search for verses containing "Christ", followed by "Jesus" anywhere in the same verse.
 
 main(List<String> arguments) {
   if ((arguments.isNotEmpty) && (arguments.length > 3)) {
@@ -15,6 +38,7 @@ main(List<String> arguments) {
     var features = {
       "open": openBible,
       "search": searchBible,
+      "compare": compareBibles,
     };
     var feature = features[arguments[0]];
     var module = arguments[1];
@@ -48,7 +72,7 @@ Future openBible(int bibleID, String bibleModule, String referenceString) async 
   if (referenceString.isNotEmpty) {
     var bible = await loadBible(bibleID, bibleModule);
     if (bible != null) {
-      var referenceList = BibleParser("ENG").extractAllReferences(referenceString);
+      var referenceList = BibleParser().extractAllReferences(referenceString);
       if (referenceList.isNotEmpty) bible.open(referenceList);
     }
   }
@@ -58,5 +82,15 @@ Future searchBible(int bibleID, String bibleModule, String searchString) async {
   if (searchString.isNotEmpty) {
     var bible = await loadBible(bibleID, bibleModule);
     if (bible != null) bible.search(searchString);
+  }
+}
+
+Future compareBibles(int bibleID, String bibleString, String referenceString) async {
+  var bibles = Bibles();
+  var bibleList;
+  (bibleString == "ALL") ? bibleList = await bibles.getALLBibleList() : bibleList = await bibles.getValidBibleList(bibleString.split("_"));
+  if (bibleList.isNotEmpty) {
+    var referenceList = BibleParser().extractAllReferences(referenceString);
+    if (referenceList.isNotEmpty) bibles.compareVerses(referenceList, bibleList);  
   }
 }
